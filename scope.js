b6eb98f3a56deaebc3342edf71f9f6f68ad764dc
SomratIIT <101197949+SomratIIT@users.noreply.github.com>
2024-12-03 00:20:47 +0600

    let,const=>block scope

---
+++ scope.js
@@ -0,0 +1,23 @@
+let sum;
+function add(a,b)
+{
+  const total = a+b;
+  
+  if(a<b)
+  {
+     sum = a+25;
+    
+  }
+  console.log(sum);
+  
+  return total;
+}
+
+console.log(add(5,7));
+
+let i;
+for(i=0;i<5;i++)
+{
+    console.log(i);
+}
+console.log(i);
--- triple.js
+++ triple.js
@@ -1,6 +1,6 @@
-const first = 0,second = false;
+const first = 2,second = [2];
 
-if(first===second){
+if(first==second){
     console.log('Values are equal');
 }
 else 
